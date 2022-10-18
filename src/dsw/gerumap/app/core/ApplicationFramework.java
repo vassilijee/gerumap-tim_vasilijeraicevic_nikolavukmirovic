8d3238b58db4cbf57fc710abd42f4928a3d0ed9e
ker ili zivotinja <nvukmirovic4121rn@raf.rs>
2022-10-18 16:32:25 +0200

    KOMIT

---
+++ src/dsw/gerumap/app/core/ApplicationFramework.java
@@ -0,0 +1,10 @@
+package dsw.gerumap.app.core;
+
+public abstract class ApplicationFramework {
+    protected Gui gui;
+    public abstract void run();
+
+    public void initialise(Gui gui){
+        this.gui = gui;
+    }
+}

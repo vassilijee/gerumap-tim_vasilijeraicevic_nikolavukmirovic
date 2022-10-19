2bfc7de69e953187f69ece32b91da14d10062006
Vasilije <vraicevic9022rn@raf.rs>
2022-10-19 20:12:46 +0200

    AbstractAction loadIcon metoda

--- src/dsw/gerumap/app/gui/swing/controller/AbstractGerumapAction.java
+++ src/dsw/gerumap/app/gui/swing/controller/AbstractGerumapAction.java
@@ -1,4 +1,27 @@
 package dsw.gerumap.app.gui.swing.controller;
 
-public class AbstractGerumapAction {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+
+public class AbstractGerumapAction extends AbstractAction {
+
+    public Icon loadIcon(String fileName){
+
+        URL imageURL = getClass().getResource(fileName);
+        Icon icon = null;
+
+        if (imageURL != null) {
+            icon = new ImageIcon(imageURL);
+        }
+        else {
+            System.err.println("Resource not found: " + fileName);
+        }
+        return icon;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }

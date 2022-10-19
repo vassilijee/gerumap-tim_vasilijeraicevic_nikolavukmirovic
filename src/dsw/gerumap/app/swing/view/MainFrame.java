70b795a7e3014d6c17891d13737672ab160d8fb7
Vasilije <vraicevic9022rn@raf.rs>
2022-10-19 14:30:49 +0200

    Menu

--- src/dsw/gerumap/app/swing/view/MainFrame.java
+++ src/dsw/gerumap/app/swing/view/MainFrame.java
@@ -1,11 +1,23 @@
 package dsw.gerumap.app.swing.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
-    public MainFrame(){
+    public MainFrame() {
         super();
-        setSize(300,200);
+        setSize(300, 200);
         setTitle("GUI");
+
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(screenWidth / 2, screenHeight / 2);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        MyMenuBar menu=new MyMenuBar();
+        this.setJMenuBar(menu);
     }
 }
--- src/dsw/gerumap/app/swing/view/MyMenuBar.java
+++ src/dsw/gerumap/app/swing/view/MyMenuBar.java
@@ -1,4 +1,20 @@
 package dsw.gerumap.app.swing.view;
 
-public class MyMenuBar {
+import javax.swing.*;
+
+public class MyMenuBar extends JMenuBar {
+    public MyMenuBar() {
+
+        JMenu file = new JMenu("File");
+        JMenuItem miNew = new JMenuItem("New");
+
+        JMenu help = new JMenu("Help");
+        JMenuItem miEdit = new JMenuItem("Edit");
+
+        file.add(miNew);
+        help.add(miEdit);
+
+        add(file);
+        add(help);
+    }
 }

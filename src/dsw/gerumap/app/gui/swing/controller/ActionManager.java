46fcefec35e59805d135281c06389573090c5b09
Vasilije <vraicevic9022rn@raf.rs>
2022-10-23 13:45:37 +0200

    ActionManager

--- src/dsw/gerumap/app/gui/swing/controller/ActionManager.java
+++ src/dsw/gerumap/app/gui/swing/controller/ActionManager.java
@@ -1,4 +1,15 @@
 package dsw.gerumap.app.gui.swing.controller;
 
 public class ActionManager {
+    private ExitAction exitAction;
+    private NewProjectAction newProjectAction;
+
+    public ActionManager() {
+       initActions();
+    }
+
+    private void initActions(){
+        exitAction = new ExitAction();
+        newProjectAction = new NewProjectAction();
+    }
 }

bd40112de83edcc7ede78d4888f3f114716eb029
Nikola <nvukmirovic4121rn@raf.rs>
2022-11-01 16:30:27 +0100

    Ikonice

--- src/main/java/dsw/gerumap/app/gui/swing/controller/RenameAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/RenameAction.java
@@ -3,10 +3,12 @@ package dsw.gerumap.app.gui.swing.controller;
 import java.awt.event.ActionEvent;
 
 public class RenameAction extends AbstractGerumapAction{
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        //putValue(SMALL_ICON);
+    public RenameAction(){
+        putValue(SMALL_ICON, loadIcon("/images/rename.png"));
         putValue(NAME, "Rename");
         putValue(SHORT_DESCRIPTION, "Rename");
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 }

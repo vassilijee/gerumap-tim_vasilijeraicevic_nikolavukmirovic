4c1da03ba57726c7d7648558beceb016b5e22a78
vraicevic9022rn <vraiceivic9022rn@raf.rs>
2022-11-04 18:52:15 +0100

    Delete key

--- src/main/java/dsw/gerumap/app/gui/swing/controller/DeleteAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/DeleteAction.java
@@ -4,24 +4,26 @@ package dsw.gerumap.app.gui.swing.controller;
 import dsw.gerumap.app.gui.swing.tree.model.MapTreeItem;
 import dsw.gerumap.app.gui.swing.view.MainFrame;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 
+public class DeleteAction extends AbstractGerumapAction {
 
-public class DeleteAction extends AbstractGerumapAction{
-
-    public DeleteAction(){
-        //putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F4, InputEvent.ALT_DOWN_MASK));
+    public DeleteAction() {
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
         putValue(SMALL_ICON, loadIcon("/images/delete.png"));
         putValue(NAME, "Delete");
         putValue(SHORT_DESCRIPTION, "Delete");
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         MapTreeItem selected = (MapTreeItem) MainFrame.getInstance().getMapTree().getSelectedNode();
-        if(selected == null){
+        if (selected == null) {
             return;
-        }else{
+        } else {
             MainFrame.getInstance().getMapTree().removeChild(selected);
         }
     }

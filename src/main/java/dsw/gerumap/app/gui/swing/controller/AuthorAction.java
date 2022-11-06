29c76a24bfbd0c4ffc47e7782b4c5cad9f9371d6
Nikola <nvukmirovic4121rn@raf.rs>
2022-11-06 22:06:29 +0100

    Author action

--- src/main/java/dsw/gerumap/app/gui/swing/controller/AuthorAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/AuthorAction.java
@@ -1,6 +1,16 @@
 package dsw.gerumap.app.gui.swing.controller;
 
+import dsw.gerumap.app.gui.swing.tree.model.MapTreeItem;
+import dsw.gerumap.app.gui.swing.view.MainFrame;
+import dsw.gerumap.app.repository.composite.MapNodeComposite;
+import dsw.gerumap.app.repository.implementation.MindMap;
+import dsw.gerumap.app.repository.implementation.Project;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class AuthorAction extends AbstractGerumapAction{
 
@@ -12,6 +22,27 @@ public class AuthorAction extends AbstractGerumapAction{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-    }
+//        MapTreeItem selected = MainFrame.getInstance().getMapTree().getSelectedNode();
+//        String text = selected.getAuthor();
+//        JTextField field = new JTextField(text, 20);
+//        JOptionPane pane = new JOptionPane(field, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null);
+//        JDialog dialog = pane.createDialog("Author");
+//        dialog.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowOpened(WindowEvent e) {
+//                field.requestFocus();
+//                field.selectAll();
+//            }
+//        });
+//        dialog.setVisible(true);
+//        dialog.dispose();
+//        Object value = pane.getValue();
+//        if (value instanceof Integer) {
+//            int result = (int) value;
+//            if (result == JOptionPane.OK_OPTION) {
+//                String newName = field.getText();
+//                MainFrame.getInstance().getMapTree().renameAuthor(selected, newName);
+//            }
+//        }
+      }
 }

a9bdfc36f2cb31d010a41b1e3d3bf815d87cfa80
Nikola <nvukmirovic4121rn@raf.rs>
2022-11-12 00:19:03 +0100

    delete observer

--- src/main/java/dsw/gerumap/app/gui/swing/view/ProjectView.java
+++ src/main/java/dsw/gerumap/app/gui/swing/view/ProjectView.java
@@ -1,5 +1,6 @@
 package dsw.gerumap.app.gui.swing.view;
 
+import dsw.gerumap.app.gui.swing.tree.model.MapTreeItem;
 import dsw.gerumap.app.observer.IPublisher;
 import dsw.gerumap.app.observer.ISubscriber;
 import dsw.gerumap.app.repository.implementation.Project;
@@ -11,7 +12,7 @@ import java.awt.*;
 @Setter
 @Getter
 
-public class ProjectView extends JFrame implements ISubscriber {
+public class ProjectView extends JPanel implements ISubscriber {
     private TabbedPane tabbedPane;
     private JLabel projectName;
     private JScrollPane scroll;
@@ -23,7 +24,7 @@ public class ProjectView extends JFrame implements ISubscriber {
         scroll = new JScrollPane(projectExplorer);
         scroll.setMinimumSize(new Dimension(200, 150));
         split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scroll, desktop);
-        getContentPane().add(split, BorderLayout.CENTER);
+        //getContentPane().add(split, BorderLayout.CENTER);
         split.setDividerLocation(250);
         split.setOneTouchExpandable(true);
         projectName = new JLabel("", JLabel.CENTER);
@@ -38,7 +39,6 @@ public class ProjectView extends JFrame implements ISubscriber {
             Project project = (Project) iPublisher;
             MainFrame.getInstance().getProjectView().getProjectName().setText(project.getName()+  " Autor: "+ project.getAuthor());
         }else if(notification.equals("DELETE")){
-            MainFrame.getInstance().getProjectView();
         }else if(notification.equals("AUTHOR")){
             Project project = (Project) iPublisher;
             MainFrame.getInstance().getProjectView().getProjectName().setText(project.getName() + " Autor: "+ project.getAuthor());

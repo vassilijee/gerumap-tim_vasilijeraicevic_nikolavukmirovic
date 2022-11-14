e77ea04ca606024a33f9a5bb9b8cbeede29d5ad7
Vasilije <vraicevic9022rn@raf.rs>
2022-11-14 20:25:03 +0100

    NodeFactory

--- src/main/java/dsw/gerumap/app/factory/ProjectFactory.java
+++ src/main/java/dsw/gerumap/app/factory/ProjectFactory.java
@@ -4,7 +4,6 @@ import dsw.gerumap.app.repository.composite.MapNode;
 import dsw.gerumap.app.repository.implementation.Project;
 
 public class ProjectFactory extends NodeFactory{
-    private int projectCount = 0;
     @Override
     public MapNode createNode(MapNode parent) {
         return new Project("Project", parent);

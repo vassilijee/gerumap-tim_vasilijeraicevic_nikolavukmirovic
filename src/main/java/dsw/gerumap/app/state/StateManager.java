57cca873533f8a7ba3103b57a2c37afc991e4a6e
Nikola <nvukmirovic4121rn@raf.rs>
2022-12-10 23:22:43 +0100

    Geteri i seteri

--- src/main/java/dsw/gerumap/app/state/StateManager.java
+++ src/main/java/dsw/gerumap/app/state/StateManager.java
@@ -1,7 +1,11 @@
 package dsw.gerumap.app.state;
 
 import dsw.gerumap.app.state.concrete.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class StateManager {
     private State currState;
     private AddLinkState addLinkState;
@@ -20,6 +24,30 @@ public class StateManager {
         deleteState = new DeleteState();
         moveState = new MoveState();
         selectState = new SelectState();
+        currState = selectState;
+    }
+
+    public State getCurrent(){
+        return currState;
+    }
+
+    public void setAddLinkState(AddLinkState addLinkState) {
+        currState = addLinkState;
+    }
+
+    public void setAddTopicState(AddTopicState addTopicState) {
+        currState = addTopicState;
+    }
+
+    public void setDeleteState(DeleteState deleteState) {
+        currState = deleteState;
+    }
+
+    public void setMoveState(MoveState moveState) {
+        currState = moveState;
+    }
+
+    public void setSelectState(SelectState selectState) {
         currState = selectState;
     }
 }

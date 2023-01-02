076b6310b13c4a58f18cdad15732b30914fe53e8
Nikola <nvukmirovic4121rn@raf.rs>
2023-01-02 14:31:41 +0100

    Move radi nekako i undo i redo

--- src/main/java/dsw/gerumap/app/repository/command/implementation/MoveCommand.java
+++ src/main/java/dsw/gerumap/app/repository/command/implementation/MoveCommand.java
@@ -1,15 +1,60 @@
 package dsw.gerumap.app.repository.command.implementation;
 
 import dsw.gerumap.app.repository.command.AbstractCommand;
+import dsw.gerumap.app.repository.implementation.Element;
+import dsw.gerumap.app.repository.implementation.Link;
+import dsw.gerumap.app.repository.implementation.MapSelectionModel;
+import dsw.gerumap.app.repository.implementation.Topic;
+
+import java.util.HashMap;
 
 public class MoveCommand extends AbstractCommand {
+    private MapSelectionModel mapSelectionModel;
+    private HashMap<String, Integer> cordX;
+    private HashMap<String, Integer> cordY;
+    private int x1, y1;
+
+    public MoveCommand(MapSelectionModel mapSelectionModel, HashMap<String, Integer> cordX, HashMap<String, Integer> cordY, int x1, int y1) {
+        this.mapSelectionModel = mapSelectionModel;
+        this.cordX = new HashMap<>();
+        this.cordY = new HashMap<>();
+        this.cordX.putAll(cordX);
+        this.cordY.putAll(cordY);
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
     @Override
     public void undoCommand() {
-
+        for (Element element:
+             mapSelectionModel.getSelected()) {
+            if(element instanceof Topic){
+                Topic topic = (Topic) element;
+                int x;
+                int y;
+                x = cordX.get(topic.getName());
+                System.out.println(x);
+                y = cordY.get(topic.getName());
+                System.out.println(y);
+                topic.setXY(x, y);
+            }else if(element instanceof Link){
+                Link link = (Link) element;
+                link.move();
+            }
+        }
     }
 
     @Override
     public void redoCommand() {
-
+        for (Element element:
+             mapSelectionModel.getSelected()) {
+            if(element instanceof Topic){
+                Topic topic = (Topic) element;
+                topic.setXY(x1, y1);
+            }else if(element instanceof Link){
+                Link link = (Link) element;
+                link.move();
+            }
+        }
     }
 }
--- src/main/java/dsw/gerumap/app/state/concrete/MoveState.java
+++ src/main/java/dsw/gerumap/app/state/concrete/MoveState.java
@@ -4,22 +4,41 @@ import dsw.gerumap.app.gui.swing.view.MindMapView;
 import dsw.gerumap.app.gui.swing.view.painters.ElementView;
 import dsw.gerumap.app.gui.swing.view.painters.LinkView;
 import dsw.gerumap.app.gui.swing.view.painters.TopicView;
+import dsw.gerumap.app.repository.command.AbstractCommand;
+import dsw.gerumap.app.repository.command.implementation.MoveCommand;
 import dsw.gerumap.app.repository.implementation.Element;
 import dsw.gerumap.app.repository.implementation.Link;
 import dsw.gerumap.app.repository.implementation.Topic;
 import dsw.gerumap.app.state.State;
 
+import java.util.HashMap;
+
 public class MoveState extends State {
     private static int clickedX;
     private static int clickedY;
+    private static HashMap<String, Integer> cordX = new HashMap<>();
+    private static HashMap<String, Integer> cordY = new HashMap<>();
+    private static int x1;
+    private static int y1;
     @Override
     public void clickedMouse(int x, int y, MindMapView m) {
         clickedX = x;
         clickedY = y;
+        if(!(m.getMapSelectionModel().getSelected().isEmpty())){
+            for (Element element:
+                 m.getMapSelectionModel().getSelected()) {
+                if(element instanceof Topic){
+                    Topic topic = (Topic) element;
+                    cordX.put(topic.getName(), topic.getX());
+                    cordY.put(topic.getName(), topic.getY());
+                }
+            }
+        }
     }
 
     @Override
     public void draggedMouse(int x, int y, MindMapView m) {
+
         if(!(m.getMapSelectionModel().getSelected().isEmpty())){
             for (Element element:
                  m.getMapSelectionModel().getSelected()) {
@@ -27,8 +46,8 @@ public class MoveState extends State {
                     Topic topic = (Topic) element;
                     int locationX = topic.getX();
                     int locationY = topic.getY();
-                    int x1 = locationX + (x - clickedX);
-                    int y1 = locationY + (y - clickedY);
+                    x1 = locationX + (x - clickedX);
+                    y1 = locationY + (y - clickedY);
                     topic.setXY(x1, y1);
                 }else if(element instanceof Link){
                     Link link = (Link) element;
@@ -51,8 +70,8 @@ public class MoveState extends State {
                     Topic topic = (Topic) elementView.getElement();
                     int locationX = topic.getX();
                     int locationY = topic.getY();
-                    int x1 = locationX + (x - clickedX);
-                    int y1 = locationY + (y - clickedY);
+                    x1 = locationX + (x - clickedX);
+                    y1 = locationY + (y - clickedY);
                     topic.setXY(x1, y1);
                 }else if(elementView instanceof LinkView){
                     Link link = (Link) elementView.getElement();
@@ -66,5 +85,7 @@ public class MoveState extends State {
 
     @Override
     public void releasedMouse(int x, int y, MindMapView m) {
+        AbstractCommand abstractCommand = new MoveCommand(m.getMapSelectionModel(), cordX, cordY, x1, y1);
+        m.getMindMap().getCommandManager().addCommand(abstractCommand);
     }
 }

8814c4f28711231d8a4ef78246eb1bd26f8af974
Vasilije <vraicevic9022rn@raf.rs>
2023-01-03 16:30:07 +0100

    export png mape sa jFileChooser

--- src/main/java/dsw/gerumap/app/gui/swing/controller/ExportAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/ExportAction.java
@@ -4,10 +4,12 @@ import dsw.gerumap.app.gui.swing.view.MainFrame;
 import dsw.gerumap.app.gui.swing.view.MindMapView;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 public class ExportAction extends AbstractGerumapAction {
     public ExportAction() {
@@ -21,16 +23,21 @@ public class ExportAction extends AbstractGerumapAction {
     public void actionPerformed(ActionEvent actionEvent) {
         int index = MainFrame.getInstance().getProjectView().getTabbedPane().getSelectedIndex();
         MindMapView mindMapView = (MindMapView) MainFrame.getInstance().getProjectView().getTabbedPane().getComponentAt(index);
-        String name = mindMapView.getName();
 
         BufferedImage image = new BufferedImage(mindMapView.getWidth(), mindMapView.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics2D g2 = image.createGraphics();
         mindMapView.paint(g2);
-        try {
-            ImageIO.write(image, "png", new File(name + "." + "png"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        g2.dispose();
 
+        JFileChooser jFileChooser = new JFileChooser();
+
+        if (jFileChooser.showSaveDialog(MainFrame.getInstance()) == JFileChooser.APPROVE_OPTION) {
+            File file = jFileChooser.getSelectedFile();
+            try {
+                ImageIO.write(image, "png", file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
